a812985fbb265b05f31c5c076c445569f4c4cef9
Danue1 <me@danuel.io>
2024-08-06 17:24:22 +0900

    Refactor

--- crates/openhwp_parser/src/document.rs
+++ crates/openhwp_parser/src/document.rs
@@ -1,6 +1,6 @@
-use crate::{section, DocInfo, FileHeader, Section};
+use crate::{DocInfo, FileHeader, Section};
 use cfb::CompoundFile;
-use std::fs::File;
+use std::{fs::File, path::Path};
 
 #[derive(Debug)]
 pub struct HwpDocument {
@@ -25,8 +25,6 @@ pub enum HwpError {
     CannotFindSections(std::io::Error),
     #[error("Cannot find section: {0}")]
     CannotFindSection(std::io::Error),
-    #[error("Malformed section: {0}")]
-    MalformedSection(std::io::Error),
     #[error("Invalid section: {0}")]
     Section(#[from] crate::SectionError),
 }
@@ -51,11 +49,8 @@ impl HwpDocument {
             .collect();
         let mut sections = vec![];
         for section in storages {
-            let mut stream = file
-                .open_stream(section.path())
-                .map_err(HwpError::CannotFindSections)?;
-            let section = read_to_end(&mut stream).map_err(HwpError::MalformedSection)?;
-            sections.push(Section::from_buf(&section).map_err(HwpError::Section)?);
+            let section = read(&mut file, section.path()).map_err(HwpError::CannotFindSection)?;
+            sections.push(Section::from_vec(section)?);
         }
 
         Ok(Self {
@@ -66,17 +61,17 @@ impl HwpDocument {
     }
 }
 
-fn read(file: &mut CompoundFile<File>, name: &str) -> Result<Vec<u8>, std::io::Error> {
-    let mut stream = file.open_stream(name)?;
+fn read<P: AsRef<Path>>(file: &mut CompoundFile<File>, path: P) -> Result<Vec<u8>, std::io::Error> {
+    #[inline(never)]
+    fn read_to_end(file: &mut CompoundFile<File>, path: &Path) -> Result<Vec<u8>, std::io::Error> {
+        use std::io::Read;
 
-    read_to_end(&mut stream)
-}
+        let mut stream = file.open_stream(path)?;
+        let mut buf = vec![];
+        stream.read_to_end(&mut buf)?;
 
-fn read_to_end(stream: &mut cfb::Stream<File>) -> Result<Vec<u8>, std::io::Error> {
-    use std::io::Read;
+        Ok(buf)
+    }
 
-    let mut buf = vec![];
-    stream.read_to_end(&mut buf)?;
-
-    Ok(buf)
+    read_to_end(file, path.as_ref())
 }
